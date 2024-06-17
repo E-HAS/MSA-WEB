852766a58d9e2c2c7867c1c1810b66369da18f40
HJH <woghfot1996@gmail.com>
2024-06-17 18:51:31 +0900

    serivce discovery 추가

---
+++ center/src/test/java/com/ehas/center/CenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehas.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
