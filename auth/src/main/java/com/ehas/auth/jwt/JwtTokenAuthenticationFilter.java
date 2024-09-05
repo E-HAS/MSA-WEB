306a6a77070a11380de99b47a387c2ae0c5fde2f
HJH <woghfot1996@gmail.com>
2024-09-05 18:50:17 +0900

    Update JwtTokenAuthenticationFilter.java

--- auth/src/main/java/com/ehas/auth/jwt/JwtTokenAuthenticationFilter.java
+++ auth/src/main/java/com/ehas/auth/jwt/JwtTokenAuthenticationFilter.java
@@ -3,27 +3,22 @@ package com.ehas.auth.jwt;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import reactor.core.publisher.Mono;
-import reactor.util.context.Context;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -47,14 +42,29 @@ public class JwtTokenAuthenticationFilter implements WebFilter  {
 			    
 			}
 		} catch (Exception e) {
+			/*
 			ServerHttpResponse response = exchange.getResponse();
 			response.setStatusCode(HttpStatus.BAD_REQUEST);
 			
 			return response.setComplete();
+			*/
+			return onError(exchange, "UNAUHORIZATION", HttpStatus.UNAUTHORIZED);
 		}
         return chain.filter(exchange);
     }
 
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        Map<String, String> responseBody = Map.of(
+        										"code",httpStatus.toString()
+        										,"msg",err.toString());
+
+        byte[] bytes = responseBody.toString().getBytes(StandardCharsets.UTF_8);
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+        return response.writeWith(Mono.just(buffer));
+    }
 		// 요청으로부터 JWT 토큰을 얻는다.
     private String resolveToken(ServerHttpRequest request) {
     	System.out.println(">>>> reslove before " + request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION));
